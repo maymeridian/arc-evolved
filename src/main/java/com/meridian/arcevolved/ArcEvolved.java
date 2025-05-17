3e2afc4300520395b8f46a2cf578d752d9ed57dc
arcindev <maximusbshort@gmail.com>
2025-05-17 19:24:14 -0400

    Initial commit

---
+++ src/main/java/com/meridian/arcevolved/ArcEvolved.java
@@ -0,0 +1,18 @@
+package com.meridian.arcevolved;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArcEvolved implements ModInitializer
+{
+	public static final String MOD_ID = "arcevolved";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize()
+	{
+	
+	}
+}
---
+++ src/main/java/com/meridian/arcevolved/ArcEvolvedClient.java
@@ -0,0 +1,14 @@
+package com.meridian.arcevolved;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcEvolvedClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient() 
+    {
+        
+    }
+    
+}
