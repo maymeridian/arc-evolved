0c5d240c1dd80df1ed131ae700cab566889c3692
maymeridian <emiliaashort@gmail.com>
2025-05-17 20:10:15 -0400

    sync

--- src/main/java/com/meridian/arcevolved/ArcEvolved.java
+++ src/main/java/com/meridian/arcevolved/ArcEvolved.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class ArcEvolved implements ModInitializer
 {
+	//test
 	public static final String MOD_ID = "arcevolved";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
